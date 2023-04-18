7ed3f56ff03c7bdd2581736df253e9eccec341d5
AIIA\itd.magang01 <itd.magang01@aiia.co.id>
2023-04-18 11:47:27 +0700

    Fix responsive nav link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Dashboards') }}
 
                     </x-nav-link>
                     <x-nav-link :href="route('checksheet.list')" :active="request()->routeIs('checksheet.list')">
@@ -83,6 +83,14 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('checksheet.list')" :active="request()->routeIs('checksheet.list')">
+                {{ __('Checksheet List') }}
+
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('checksheet.data')" :active="request()->routeIs('checksheet.data')">
+                {{ __('Checksheet Data') }}
+
             </x-responsive-nav-link>
         </div>
 
