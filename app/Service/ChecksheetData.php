50ab34cb7497c4548dcdc5b6f67de63be4521a05
AIIA\itd.magang01 <itd.magang01@aiia.co.id>
2023-05-31 08:59:10 +0700

    Cache line setiap 5 menit agar tidak boros query

--- app/Service/ChecksheetData.php
+++ app/Service/ChecksheetData.php
@@ -3,46 +3,57 @@
 namespace App\Service;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+
 class ChecksheetData
 {
     public function getLine()
     {
-        $line = DB::table('tm_checksheet')
-        ->select('line')
-        ->distinct()
-        ->orderBy('line','ASC')
-        ->get();
+
+        $line = Cache::remember('getLine', 300, function () {
+            return DB::table('tm_checksheet')
+                ->select('line')
+                ->distinct()
+                ->orderBy('line', 'ASC')
+                ->get();
+        });
         return $line;
     }
     public function getCode($line)
     {
-        $line = DB::table('tm_checksheet')
-        ->select('code')
-        ->distinct()
-        ->where('line',$line)
-        ->orderBy('code','ASC')
-        ->get();
-        return $line;
+        $model = Cache::remember('getCode' . $line, 300, function () use ($line) {
+            return DB::table('tm_checksheet')
+                ->select('code')
+                ->distinct()
+                ->where('line', $line)
+                ->orderBy('code', 'ASC')
+                ->get();
+        });
+        return $model;
     }
-    public function getChecksheet($line,$code)
+    public function getChecksheet($line, $code)
     {
-        $sheet = DB::table('tm_checksheet')
-        ->select('id','nama')
-        ->distinct()
-        ->where('line',$line)
-        ->where('code',$code)
-        ->orderBy('id','ASC')
-        ->get();
+        $sheet = Cache::remember('getChecksheet' . $line . $code, 300, function () use ($line, $code) {
+            return DB::table('tm_checksheet')
+                ->select('id', 'nama')
+                ->distinct()
+                ->where('line', $line)
+                ->where('code', $code)
+                ->orderBy('id', 'ASC')
+                ->get();
+        });
         return $sheet;
     }
-    public function getArea($line,$code,$idchecksheet)
+    public function getArea($line, $code, $idchecksheet)
     {
-        $area = DB::table('tm_checkarea')
-        ->select('id','nama')
-        ->distinct()
-        ->where('id_checksheet',$idchecksheet)
-        ->orderBy('id','ASC')
-        ->get();
+        $area = Cache::remember('getArea' . $line . $code . $idchecksheet, 300, function () use ($line, $code, $idchecksheet) {
+            return DB::table('tm_checkarea')
+                ->select('id', 'nama')
+                ->distinct()
+                ->where('id_checksheet', $idchecksheet)
+                ->orderBy('id', 'ASC')
+                ->get();
+        });
         return $area;
     }
 }
