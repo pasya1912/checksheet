9e3a08a531f50feb0f57461055c49e941cd60a44
AIIA\itd.magang01 <itd.magang01@aiia.co.id>
2023-05-31 08:17:31 +0700

    Frontend dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,54 +10,90 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1 class="text-3xl font-bold text-center">AIIA</h1>
-                    <div class="w-full h-max-7xl flex flex-wrap my-1">
-                        <div class="w-4/12 card border my-1 p-2">
+                    <h1 class="text-4xl font-bold text-center">AIIA</h1>
+                    <div class="w-full h-max-7xl flex lg:justify-between justify-center flex-wrap my-1">
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
                             <div class="header-card text-2xl text-center font-bold">
                                 <span>MA001</span>
                             </div>
-                            <div class="body-card">
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
+                            <div class="body-card my-2 w-full">
+                                <div class="my-1 w-full"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block ">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
                             </div>
                         </div>
-                        <div class="w-4/12 card border my-1 p-2">
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
                             <div class="header-card text-2xl text-center font-bold">
                                 <span>MA001</span>
                             </div>
                             <div class="body-card">
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
                             </div>
                         </div>
-                        <div class="w-4/12 card border my-1 p-2">
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
                             <div class="header-card text-2xl text-center font-bold">
                                 <span>MA001</span>
                             </div>
                             <div class="body-card">
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
                             </div>
                         </div>
-                        <div class="w-4/12 card border my-1 p-2">
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
                             <div class="header-card text-2xl text-center font-bold">
                                 <span>MA001</span>
                             </div>
                             <div class="body-card">
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
-                                <div>889F</div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                            </div>
+                        </div>
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
+                            <div class="header-card text-2xl text-center font-bold">
+                                <span>MA001</span>
+                            </div>
+                            <div class="body-card">
+                                <div class="my-1"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                            </div>
+                        </div>
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
+                            <div class="header-card text-2xl text-center font-bold">
+                                <span>MA001</span>
+                            </div>
+                            <div class="body-card">
+                                <div class="my-1"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                            </div>
+                        </div>
+                        <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
+                            <div class="header-card text-2xl text-center font-bold">
+                                <span>MA001</span>
+                            </div>
+                            <div class="body-card">
+                                <div class="my-1"><span>889F</span> <span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span> <span class="bg bg-red-300 px-1 py-0.5 rounded-md inline-block">1 NG</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
+                                <div class="my-1"><span>889F</span><span class="bg bg-green-300 px-1 py-0.5 rounded-md inline-block">OK (1 Revised)</span></div>
                             </div>
                         </div>
                     </div>
