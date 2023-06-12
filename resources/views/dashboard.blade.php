da443a249bcfdf1e7e7ea89d74e5c249265f64a2
AIIA\itd.magang01 <itd.magang01@aiia.co.id>
2023-06-12 15:07:25 +0700

    add tombol dibawah

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,20 +15,22 @@
                 <div class="p-6 text-gray-900">
                     <h1 class="text-4xl font-bold text-center">AIIA</h1>
                     <h2>*Status berikut merupakan data dihari ini</h2>
-                    <div class="w-full h-max-7xl flex justify-center">
+                    <div class="w-full h-max-7xl flex flex-col justify-center">
+                        <div class="text-xl text-center font-bold">
+                            <span>Overall Chart</span>
+                        </div>
+
                         <div class="w-11/12 md:w-6/12" id="chart"></div>
                     </div>
                     <div class="w-full h-max-7xl flex lg:justify-between justify-center flex-wrap my-1">
 
                         @foreach ($daget as $key => $data)
                             <div class="w-6/12 lg:w-3/12 card border my-1 p-2">
-                                <div class="header-card text-2xl text-center font-bold">
+                                <div class="header-card text-xl text-center font-bold">
                                     <span>{{ $data['line'] }}</span>
                                 </div>
                                 <div class="body-card my-2 w-full">
-
-                                        <div id="chart{{$key}}"></div>
-
+                                    <div id="chart{{ $key }}"></div>
                                 </div>
                             </div>
                         @endforeach
@@ -39,7 +41,7 @@
     </div>
     @section('script')
         <script>
-                        var options = {
+            var options = {
                 series: [{
                     name: 'Good',
                     data: {!! json_encode($allArray['status']['ok']) !!},
@@ -86,7 +88,7 @@
                 tooltip: {
                     y: {
                         formatter: function(val) {
-                            return  val + " Data"
+                            return val + " Data"
                         }
                     }
                 }
@@ -97,61 +99,61 @@
 
             @foreach ($daget as $key => $datass)
 
-            var options = {
-                series: [{
-                    name: 'Good',
-                    data: {!! json_encode($daget[$key]['status']['ok']) !!},
-                    color: '#81C784'
-                }, {
-                    name: 'NG',
-                    data: {!! json_encode($daget[$key]['status']['ng']) !!},
-                    color: '#E57373'
-                }, {
-                    name: 'Revised  ',
-                    data: {!! json_encode($daget[$key]['status']['revised']) !!},
-                    color: '#E0E0E0'
-                }],
-                chart: {
-                    type: 'bar',
-                    height: 350
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        columnWidth: '55%',
-                        endingShape: 'rounded'
+                var options = {
+                    series: [{
+                        name: 'Good',
+                        data: {!! json_encode($daget[$key]['status']['ok']) !!},
+                        color: '#81C784'
+                    }, {
+                        name: 'NG',
+                        data: {!! json_encode($daget[$key]['status']['ng']) !!},
+                        color: '#E57373'
+                    }, {
+                        name: 'Revised  ',
+                        data: {!! json_encode($daget[$key]['status']['revised']) !!},
+                        color: '#E0E0E0'
+                    }],
+                    chart: {
+                        type: 'bar',
+                        height: 350
                     },
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                stroke: {
-                    show: true,
-                    width: 2,
-                    colors: ['transparent']
-                },
-                xaxis: {
-                    categories: {!! json_encode($daget[$key]['model']) !!},
-                },
-                yaxis: {
-                    title: {
-                        text: '{{$daget[$key]['line']}}'
-                    }
-                },
-                fill: {
-                    opacity: 1
-                },
-                tooltip: {
-                    y: {
-                        formatter: function(val) {
-                            return  val + " Data"
+                    plotOptions: {
+                        bar: {
+                            horizontal: false,
+                            columnWidth: '55%',
+                            endingShape: 'rounded'
+                        },
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    stroke: {
+                        show: true,
+                        width: 2,
+                        colors: ['transparent']
+                    },
+                    xaxis: {
+                        categories: {!! json_encode($daget[$key]['model']) !!},
+                    },
+                    yaxis: {
+                        title: {
+                            text: '{{ $daget[$key]['line'] }}'
+                        }
+                    },
+                    fill: {
+                        opacity: 1
+                    },
+                    tooltip: {
+                        y: {
+                            formatter: function(val) {
+                                return val + " Data"
+                            }
                         }
                     }
-                }
-            };
+                };
 
-            var chart = new ApexCharts(document.querySelector("#chart{{$key}}"), options);
-            chart.render();
+                var chart = new ApexCharts(document.querySelector("#chart{{ $key }}"), options);
+                chart.render();
             @endforeach
         </script>
     @endsection
