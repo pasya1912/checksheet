94bac37ca1bfe9b7cea1e898afcdcaec026ee511
AIIA\itd.magang01 <itd.magang01@aiia.co.id>
2023-06-26 09:26:06 +0700

    pindahin logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,13 +19,13 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-96 h-40 fill-current text-gray-500" />
-                </a>
-            </div>
 
             <div class="w-7/12 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <div class="mb-5">
+                <a href="/" class="">
+                    <x-application-logo class="w-96 h-40 fill-current text-gray-500" />
+                </a>
+                </div>
                 {{ $slot }}
             </div>
         </div>
